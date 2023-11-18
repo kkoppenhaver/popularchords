30db4b4c78b8a09da58fd711aaea1e27e9dfe47e
Keanan Koppenhaver <keanan@alphaparticle.com>
2023-11-18 15:35:58 -0600

    Fixing eslint errors

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,14 @@ export default function Home() {
       </div>
 
       <div className="z-10 max-w-5xl w-full items-center flex-col justify-center text-sm lg:flex mt-4 max-w-md text-center">
-        <p>If you're looking for some common chord progressions to use in your improvization or just to make yourself a more versatile musician, look no further! Below you'll find various progressions, able to be transposed into any key, with the accompanying sheet music and songs where this progression is featured.</p>
+        <p>If you&apos;re looking for some common chord progressions to use in your improvization or just to make yourself a more versatile musician, look no further! Below you&apos;ll find various progressions, able to be transposed into any key, with the accompanying sheet music and songs where this progression is featured.</p>
 
         <p className="mt-4">If you have suggestion for this list, please email <a href="mailto:help@popularchords.com">help@popularchords.com</a></p>
       </div>
 
       <div className="flex mt-6 progression justify-between">
         <div>
-          <h2>The "Journey" Progression</h2>
+          <h2>The &quot;Journey&quot; Progression</h2>
 
           <p>I - V - vi - IV</p>
         </div>
