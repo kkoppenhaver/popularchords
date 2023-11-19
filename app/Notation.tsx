ee15b6b4955fbdafc5e2bbfa879b62a45339f57e
Keanan Koppenhaver <keanan@alphaparticle.com>
2023-11-18 22:28:18 -0600

    First working version of notation

--- app/Notation.tsx
+++ app/Notation.tsx
@@ -1,14 +1,19 @@
-'use client'
+'use client';
 
 import { useEffect, useId } from 'react';
-import abcjs from "abcjs";
+import abcjs from 'abcjs';
 
-const Notation = ( {notation} ) => {
+// Define the types for your component's props
+interface NotationProps {
+  notation: string;
+}
+
+const Notation = ({ notation }: NotationProps) => {
   const notationId = useId();
 
   useEffect(() => {
     abcjs.renderAbc(notationId, notation);
-  });
+  }, [notation, notationId]); // Added notation and notationId as dependencies
 
   return <div id={notationId} className="notation max-w-xs" data-notation={notation}></div>;
 }
