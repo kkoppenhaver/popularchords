8ddbb6a3df0a328a0163d3c46e81041b7423bf3d
Keanan Koppenhaver <keanan@alphaparticle.com>
2023-12-21 13:16:40 -0600

    Fixing linting errors

--- app/page.tsx
+++ app/page.tsx
@@ -18,13 +18,13 @@ export default function Home() {
 
         <Notation notation={'X:1\nK:C\n"C"[c4e4g4]"G"[g,4b,4d4]"Am"[a,4c4e4]"F"[f,4a,4c4]\n'} />
 
-        <p className="mt-4">I call this the Journey progression because one of the most common places you'll hear it is in Don&apos;t Stop Believin&apos; by Journey. Arguably _the_ most common progression in popular music, you'll hear this one everywhere. There's even a <a href="https://www.youtube.com/watch?v=5pidokakU4I">whole video</a> that's just a compilation of these songs stitched together.</p>
+        <p className="mt-4">I call this the Journey progression because one of the most common places you&apos;ll hear it is in Don&apos;t Stop Believin&apos; by Journey. Arguably _the_ most common progression in popular music, you&apos;ll hear this one everywhere. There&apos;s even a <a href="https://www.youtube.com/watch?v=5pidokakU4I">whole video</a> that&apos;s just a compilation of these songs stitched together.</p>
 
         <h3 className="text-lg mt-4">Examples</h3>
 
         <a href="https://www.youtube.com/watch?v=1k8craCGpgs">Don&apos;t Stop Believin&apos;, Journey</a>
         <a href="https://www.youtube.com/watch?v=QDYfEBY9NM4">Let It Be, The Beatles</a>
-        <a href="https://www.youtube.com/watch?v=EkHTsc9PU2A">I'm Yours, Jason Mraz</a> 
+        <a href="https://www.youtube.com/watch?v=EkHTsc9PU2A">I&apos;m Yours, Jason Mraz</a> 
       </div>
 
       <div className="flex mt-8 progression justify-between max-w-xl text-center flex-col items-center">
@@ -32,14 +32,14 @@ export default function Home() {
 
         <Notation notation={'X:1\nK:C\n"C"[c4e4g4]"Am"[a,4c4e4]"F"[f,4a,4c4]"G"[g,4b,4d4]\n'} />
 
-        <p className="mt-4">This progression is recognizable right away because of the natural walking down movement of the bass line. It's often, but not always, associated with slower, sadder songs.</p>
+        <p className="mt-4">This progression is recognizable right away because of the natural walking down movement of the bass line. It&apos;s often, but not always, associated with slower, sadder songs.</p>
 
         <h3 className="text-lg mt-4">Examples</h3>
 
-        <a href="https://www.youtube.com/watch?v=htOTukXy84E">She's Everything, Brad Paisley</a> 
+        <a href="https://www.youtube.com/watch?v=htOTukXy84E">She&apos;s Everything, Brad Paisley</a> 
         <a href="https://youtu.be/en2D_5TzXCA?si=tOQMPA35WBWbyTRf&amp;t=58">Million Reasons, Laday Gaga</a>
         <a href="https://www.youtube.com/watch?v=fV4DiAyExN0">The Reason, Hoobastank</a>
-        <small>(doesn't do the progression exactly because it has an<br />extra return to the I, but it's the same idea)</small>
+        <small>(not the progression exactly because it has an<br />extra return to the I, but it&apos;s the same idea)</small>
       </div>
 
       <div className="flex mt-8 progression justify-between max-w-xl text-center flex-col items-center">
@@ -47,11 +47,11 @@ export default function Home() {
 
         <Notation notation={'X:1\nK:C\n"C"[c4e4g4]"Am"[a,4c4e4]"F"[f,4a,4c4]"G"[g,4b,4d4]"Am"[a,4c4e4]"G"[g,4b,4d4]"C"[c4e4g4]\n'} />
 
-        <p className="mt-4">Similar to the walk down progression, but useful in most any progression really, when you have a V chord (G in the key of C for example) and you're not quite ready to end the progression, you can move it up to the vi (Am in the key of C). Because everyone expects the V chord to resolve to the I, this is considered "deceptive".</p>
+        <p className="mt-4">Similar to the walk down progression, but useful in most any progression really, when you have a V chord (G in the key of C for example) and you&apos;re not quite ready to end the progression, you can move it up to the vi (Am in the key of C). Because everyone expects the V chord to resolve to the I, this is considered &quot;deceptive&quot;.</p>
       </div>
 
       <div className="flex mt-8 progression justify-between max-w-xl text-center flex-col items-center">
-        <h2 className="text-xl">The "Starts on 4" (IV - V - I - vi)</h2>
+        <h2 className="text-xl">The &quot;Starts on 4&quot; (IV - V - I - vi)</h2>
 
         <Notation notation={'X:1\nK:C\n"F"[f,4a,4c4]"G"[g,4b,4d4]"C"[c4e4g4]"Am"[a,4c4e4]\n'} />
 
